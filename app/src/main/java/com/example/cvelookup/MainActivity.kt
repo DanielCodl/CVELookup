94fce20dad0900469614423281a1008616f5e9ed
DCODL <dd@dd.com>
2024-01-26 15:34:28 +0100

    Updated MainActivity.kt with Toast message to check for non-empty CVE when searching

--- app/src/main/java/com/example/cvelookup/MainActivity.kt
+++ app/src/main/java/com/example/cvelookup/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.cvelookup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.cvelookup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,10 +22,16 @@ class MainActivity : AppCompatActivity() {
         val tv_cve_date_view = binding.tvCveDate // findViewById<TextView>(R.id.tv_cve_date)
         val tv_cve_desc_view = binding.tvCveDesc // findViewById<TextView>(R.id.tv_cve_desc)
         val bt_submit_cve_button = binding.btSubmitCve // findViewById<Button>(R.id.bt_submit_cve)
+
         bt_submit_cve_button.setOnClickListener() {
+            if (binding.evCveNum.text.isEmpty()){
+                Toast.makeText(this@MainActivity, "CVE number cannot be empty!", Toast.LENGTH_SHORT).show()
+            } else{
+            binding.evCveNum.text.clear()
             tv_cve_num_view.text= binding.evCveNum.text.toString() // findViewById<EditText>(R.id.ev_cve_num).text.toString()
             tv_cve_date_view.text=getString(R.string.cve_date_test)
             tv_cve_desc_view.text=getString(R.string.cve_desc_test)
+            }
         }
     }
 }
